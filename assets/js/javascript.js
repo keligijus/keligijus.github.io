d1629b03050fc03cd94737d321adeca590d8f2ed
keligijus <k.eligijus@gmail.com>
2014-11-17 09:03:03 +0200

    analytics and webmaster conf. added

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -11,10 +11,13 @@
 
 console.log("Hello World");
 
+
+//Lazyload
 $("div.lazy").lazyload({
   effect : "fadeIn"
 });
 
+//Smooth scrool
 smoothScroll.init({
 	speed: 1111, 
 	easing: 'easeInOutCubic',
@@ -22,6 +25,7 @@ smoothScroll.init({
 	offset: 0,
 });
 
+//Rotating words
 setInterval(function(){
 		var $this = $('span.skills.visible');
 		$this.removeClass('visible');
@@ -35,3 +39,12 @@ setInterval(function(){
 			$('span#first-skill').addClass('visible');
 		}
 },750);
+
+//Google Analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56813929-1', 'auto');
+  ga('send', 'pageview');
